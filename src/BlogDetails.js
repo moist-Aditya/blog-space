87500ded8deda7d1f45a46e486ae0f229ff9d8ab
Aditya Sharma <aditya.wob@proton.me>
2024-01-14 16:53:31 +0530

    Added create blog and delete blog functionalities.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import { Fragment } from "react";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch(`http://192.168.242.240:8000/blogs/${id}`);
+    const navigate = useNavigate();
 
     const renderBodyWithLineBreaks = () => {
         return blog.body.split('\n').map((paragraph, index) => (
@@ -13,6 +14,15 @@ const BlogDetails = () => {
             <br />
           </Fragment>
         ));
+    };
+
+    const handleDeleteBlog = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log("Blog deleted");
+            navigate('/');
+        });
     };
       
 
@@ -27,6 +37,8 @@ const BlogDetails = () => {
                         <h2 className="blog-title">{blog.title}</h2>
                         <p className="blog-author">By <span>{blog.author}</span></p>
                         <div className="blog-body">{renderBodyWithLineBreaks()}</div>
+
+                        <button className="btn-delete" onClick={handleDeleteBlog}>Delete Blog</button>
                     </article>
                 </div>
             )}
--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,36 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("New blog added");
+            setIsPending(false);
+            navigate('/');
+        });
+
+    };
 
     return (
         <div className="create">
             <h1 className="title">Compose a Stellar Blog Entry</h1>
 
-            <form className="flex">
+            <form className="flex" onSubmit={handleSubmit}>
                 
                 <div className="form-field flex">
                     <label>Title</label>
@@ -26,7 +47,8 @@ const Create = () => {
                     <input type="text" placeholder="Enter author" required value={author} onChange={(e) => setAuthor(e.target.value)}/>
                 </div>
                 
-                <button>Post blog</button>
+                {!isPending && <button>Post blog</button>}
+                {isPending && <button>Posting...</button>}
 
             </form>
         </div>
