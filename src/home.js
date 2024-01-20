13b3a4c026772e23ed816894c11506db020b1585
Aditya Sharma <aditya.wob@proton.me>
2024-01-20 18:20:24 +0530

    Added search bar to filter blogs on main page

--- src/home.js
+++ src/home.js
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
     const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+    const [search, setSearch] = useState('')
 
-
+    const filteredBlogs = blogs && blogs.filter((blog) =>
+        blog.title.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <div className="home">
+            {/* Search bar */}
+            <div className="search-bar flex">
+                <label>Search</label>
+                <input
+                    type="text"
+                    placeholder="Enter the title.."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </div>
+                
+
             {error && <div>Error: {error}...</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="Orbiting All Blogs..."/>}
+            {/* {blogs && <BlogList blogs={blogs} title="Orbiting All Blogs..."/>} */}
+            {filteredBlogs && <BlogList blogs={filteredBlogs} title="Orbiting All Blogs..." />}
         </div>
     );
 }
